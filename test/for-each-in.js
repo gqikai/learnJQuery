2378954cc4bc595477794812df6e7d15d4fd9871
gaoqikai <439673129@qq.com>
2016-07-16 22:05:50 +0800

    nothing to say

---
+++ test/for-each-in.js
@@ -0,0 +1,6 @@
+/**
+ * Created by gaoqikai on 7/13/16.
+ */
+for (var obj in array) {
+    
+}
---
+++ test/self-called-fucntion.js
@@ -0,0 +1,8 @@
+/**
+ * Created by gaoqikai on 7/13/16.
+ */
+(function(){})();
+
+(function(){
+    console.log('hello');
+}());
