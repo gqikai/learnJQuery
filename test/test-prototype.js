3ea86f64e6dd0ea492f9b79e6d3c13032a66b13a
gaoqikai <439673129@qq.com>
2016-07-22 10:36:18 +0800

    implemented a spping ball

--- test/test-prototype.js
+++ test/test-prototype.js
@@ -1,3 +1,9 @@
 /**
  * Created by gaoqikai on 7/21/16.
  */
+    function Foo(){};
+var foo = new Foo();
+var o = {};
+console.log(o.__proto__);
+console.log(Foo.prototype);
+console.log(foo.__proto__ === Foo.prototype);
