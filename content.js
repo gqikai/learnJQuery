7e2899fd114d18d6f5a973c933a931a1aadfdec1
gaoqikai <439673129@qq.com>
2016-07-25 20:13:50 +0800

    full of bullshit

---
+++ content.js
@@ -0,0 +1,4 @@
+/**
+ * Created by gaoqikai on 7/22/16.
+ */
+module.exports = "It works from content.js.";
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+/**
+ * Created by gaoqikai on 7/22/16.
+ */
+module.exports = {
+    entry: "./entry.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" }
+        ]
+    }
+};
